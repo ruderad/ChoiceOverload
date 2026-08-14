d1620e11178cb6d0e23ec54d28af4b20ef22d413
Radmher Bahrami <rbahrami@icloud.com>
2026-08-14 18:43:45 +0330

    taskChoice

--- Modules/runChoiceBlock.m
+++ Modules/runChoiceBlock.m
@@ -2,30 +2,31 @@ function block = runChoiceBlock(P, T, ChoiceSets, Layout)
 
 % runChoiceBlock  Run all trials in one choice-task block.
 %
+%   block = runChoiceBlock(P, T, ChoiceSets, Layout)
+%
 %   Runs every choice trial in the supplied block and stores its results.
 %
 %   Inputs:
 %       P         - Parameter structure.
 %       T         - Task structure.
-%       ChoiceSets - Cell array containing the choice set for each trial.
+%       ChoiceSets - Struct array containing the choice sets for the block.
 %       Layout    - Pixel-based choice-task layout.
 %
 %   Output:
 %       block     - Structure containing the results of each trial.
 
 
-
-
 nTrials = numel(ChoiceSets);
 
 block.trial = repmat(struct(), 1, nTrials);
+
 
 for trial = 1:nTrials
 
     block.trial(trial) = runChoiceTrial( ...
         P, ...
         T, ...
-        ChoiceSets{trial}, ...
+        ChoiceSets(trial).imageIDs, ...
         Layout);
 
 end
